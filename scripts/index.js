8a4f3392ca20d758025189bf2fbd8039ccacd01c
“Sharece” <Sharece@student.dev-island.org>
2022-05-05 20:55:57 -1000

    final changes

--- scripts/index.js
+++ scripts/index.js
@@ -1,16 +1,16 @@
-const openModalBtn = document.querySelector("contactBtn");
-const modal = document.querySelector("contactModal");
-const closeModalBtn = modal.querySelector("modal__close-btn");
+const openModalBtn = document.querySelector(".contactBtn");
+const modal = document.querySelector(".modal");
+const closeModalBtn = modal.querySelector(".modal__close-btn");
 
 function openModal() {
-  modal.classList.add("modal_open");
+  modal.classList.add("modal_is_open");
 }
 
 function closeModal() {
-  modal.classList.remove("modal_open");
+  modal.classList.remove("modal_is_open");
 }
 
-openModalBtn.addEventListener("click, openModal, true");
-closeModalBtn.addEventListener("click, closeModal, true");
+openModalBtn.addEventListener("click", openModal, false);
+closeModalBtn.addEventListener("click", closeModal, false);
 
 // class
